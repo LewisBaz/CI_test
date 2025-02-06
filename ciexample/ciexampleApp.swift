648f63f31a3378ba2b83772931daf403494dec70
Lewis <lev.bazhkov@aisa.ru>
2025-02-07 01:30:12 +0300

    init project

---
+++ ciexample/ciexampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  ciexampleApp.swift
+//  ciexample
+//
+//  Created by Lewis on 07.02.2025.
+//
+
+import SwiftUI
+
+@main
+struct ciexampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
